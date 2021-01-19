97aacc452c565b2b147c1625a2216f9f07d334ea
William Osman <wosman007@gmail.com>
2021-01-19 18:35:49 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+let express = require('express');
+let app = express();
+const sequelize = require("./db");
+
+let journal = require('./controllers/journalcontroller');
+let user = require('./controllers/usercontroller');
+let calc = require("./controllers/calulatorcontroller");
+
+sequelize.sync();
+app.use(express.json());
+
+app.use('/user', user);
+
+/*Protected route */
+
+app.use('/journal', journal);
+app.use("/calculator", calc);
+app.listen(3000, function (){
+    console.log('App is listening on port 3000');
+});
+
---
+++ server/controllers/usercontroller.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const User = require("../db").import("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+/*CREATE A USER */
+
+router.post("/create", function (req, res) {
+  User.create({
+    email: req.body.user.email,
+    password: bcrypt.hashSync(req.body.user.password, 13),
+  })
+    .then(function createSuccess(user) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: 60 * 60 * 24,
+      });
+      res.json({
+        user: user,
+        message: "User successfully created!",
+        sessionToken: token,
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+/* USER LOGIN */
+
+router.post("/login", function (req, res) {
+  User.findOne({
+    where: {
+      email: req.body.user.email,
+    },
+  })
+    .then(function loginSuccess(user) {
+      if (user) {
+        bcrypt.compare(
+          req.body.user.password,
+          user.password,
+          function (err, matches) {
+            if (matches) {
+              let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                expiresIn: 60 * 60 * 24,
+              });
+
+              res.status(200).json({
+                user: user,
+                message: "User successfully logged in!",
+                sessionToken: token,
+              });
+            } else {
+              res.status(502).send({ error: "Login Failed" });
+            }
+          }
+        );
+      } else {
+        res.status(500).json({ error: "User does not exist." });
+      }
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
