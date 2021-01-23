18ea5730664328be5301b52e1f3383311e5b9226
William Osman <wosman007@gmail.com>
2021-01-22 22:16:12 -0500

    commit 1.22.21

--- client/get-scripts.js
+++ client/get-scripts.js
@@ -88,7 +88,68 @@ function displayMine() {
      *** DISPLAY ALL ***
     ************************** */
     function displayAll() {
-     console.log('displayAll Function Called')
+     fetch('http://localhost:3000/journal/', {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(
+            function (response){
+                return response.json()
+            })
+            .catch(
+                function (error) {
+                    console.error('Error', error)
+                })
+                .then(function (response) {
+                    console.log(response)
+
+                    let display = document.getElementById('journals')
+                    for (i = 0; i = display.childNodes.length; i++){
+                        display.removeChild(display.firstChild)
+                    }
+
+                    if (response.length === 0) {
+                        let display = document.getElementById('journals')
+                        let header = document.createElement('h5')
+
+                        display.appendChild(header)
+                        header.textContent = "There are not any posts yet!"
+                        header.setAttribute("class", "noPosts")
+                    } else {
+                       for (i = 0; i < response.length; i++) {
+                           let card = document.createElement('div')
+                           let body = document.createElement('div')
+                           let header = document.createElement('h5')
+                           let subtitle = document.createElement('h6')
+                           let para = document.createElement('p')
+                           let display = document.getElementById('journals')
+
+                           let current = response[i]
+                           let title = current.title;
+                           let date = current.date;
+                           let entry = current.entry;
+
+                           display.appendChild(card)
+                           card.appendChild(body)
+                           body.appendChild(header)
+                           body.appendChild(subtitle)
+                           body.appendChild(para)
+
+                           card.setAttribute('id', current.id)
+                           card.setAttribute('class', 'card')
+                           body.setAttribute('class', 'card-body')
+                           header.setAttribute('class', 'card-title')
+                           subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted')
+                           para.setAttribute('class', 'card-text')
+
+                           header.textContent = title
+                           subtitle.textContent = date
+                           para.textContent = entry
+                       } 
+                    }
+                })
     }
     
     
@@ -96,5 +157,69 @@ function displayMine() {
      *** DISPLAY BY TITLE ***
     ************************** */
     function displayByTitle() {
-     console.log('displayByTitle Function Called')
+     let journalTitle = document.getElementById('searchBar').value;
+    console.log(journalTitle)
+    fetch(`http://localhost:3000/journal/${journalTitle}`, {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+    .then(
+        function (response) {
+            return response.json()
+        })
+        .catch(
+            function (error) {
+                console.log('Error:', error)
+            })
+            .then(function (response) {
+                console.log(response)
+
+                let display = document.getElementById('journals')
+                for (i = 0; i = display.childNodes.length; i++) {
+                    display.removeChild(display.firstChild)
+                }
+
+                if (response.length === 0) {
+                   let display = document.getElementById('journals')
+                   let header = document.createElement('h5')
+                   
+                   display.appendChild(header)
+                   header.textContent = "There are not any posts on this topic."
+                   header.setAttribute("class", "noPosts")
+                } else {
+
+                    for (i = 0; i < response.length; i++) {
+                        let card = document.createElement('div')
+                        let body = document.createElement('div')
+                        let header = document.createElement('h5')
+                        let subtitle = document.createElement('h6')
+                        let para = document.createElement('p')
+                        let display = document.getElementById ('journals')
+
+                        let current = response[i]
+                        let title = current.title;
+                        let date = current.date;
+                        let entry = current.entry;
+
+                        display.appendChild(card)
+                        card.appendChild(body)
+                        body.appendChild(header)
+                        body.appendChild(subtitle)
+                        body.appendChild(para)
+
+                        card.setAttribute('id', current.id)
+                        card.setAttribute('class', 'card')
+                        body.setAttribute('class', 'card-body')
+                        header.setAttribute('class', 'card-title')
+                        subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted')
+                        para.setAttribute('class', 'card-text')
+
+                        header.textContent = title
+                        subtitle.textContent = date
+                        para.textContent = entry
+                    }
+                }
+            })
     }
--- client/post-edit-delete-scripts.js
+++ client/post-edit-delete-scripts.js
@@ -31,7 +31,39 @@ function postJournal() {
      *** UPDATE JOURNAL ***
     ************************** */
     function editJournal(postId) {
-     console.log('editJournal Function Called')
+     console.log(postId)
+    const fetch_url = `http://localhost:3000/journal/update/${postId}`
+    const accessToken = localStorage.getItem('SessionToken')
+
+    let card = document.getElementById(postId)
+    let input = document.createElement('input')
+
+    if (card.childNodes.length < 2) {
+        card.appendChild(input)
+        input.setAttribute('type', 'text')
+        input.setAttribute('id', 'updatedEntry')
+        input.setAttribute('placeholder', 'Edit your journal entry')
+    } else {
+        let updated = document.getElementById('updatedEntry').value
+        let updateEntry = { journal: {entry: updated}};
+        const response = fetch(fetch_url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': accessToken
+            },
+            body: JSON.stringify(updateEntry)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            displayMine();
+        })
+
+        card.removeChild(card.lastChild)
+    }
     }
     
     
@@ -39,5 +71,21 @@ function postJournal() {
      *** DELETE JOURNAL ***
     ************************** */
     function deleteJournal(postId) {
-     console.log('deleteJournal Function Called')
+     console.log('deleteJournal working')
+     console.log(postId)
+
+     const fetch_url = `http://localhost:3000/journal/delete/${postId}`
+     const accessToken = localStorage.getItem('SessionToken')
+
+     fetch(fetch_url, {
+         method: 'DELETE',
+         headers: {
+             'Content-Type': 'application/json',
+             'Authorization': accessToken
+         }
+     })
+     .then(response => {
+         console.log(response);
+         displayMine()
+     })
     }
